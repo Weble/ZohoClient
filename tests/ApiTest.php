cd717dd1e6a95d8a2bda69e17fa1aaf44878546b
Skullbock <daniele@weble.it>
2021-02-03 11:36:20 +0100

    wip

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -59,7 +59,9 @@ class ApiTest extends TestCase
         $envConfig = $_SERVER['OAUTH_CONFIG'] ?? $_ENV['OAUTH_CONFIG'] ?? null;
         if ($envConfig) {
             $auth = json_decode($envConfig);
+            var_dump($auth);
         }
+        
 
         $region = Region::US;
         if ($auth->region) {
