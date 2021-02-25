1a0ff636e0c1498e19c6f0cb0f8a4f3abe1a2bcb
Andrew Nagy <anagy@clearlyip.com>
2021-02-25 13:51:27 -0500

    Fixes #11 extend Asad class to set expires correctly

---
+++ src/Oauth/Zoho.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Weble\ZohoClient\Oauth;
+
+use Asad\OAuth2\Client\Provider\Zoho as AsadZoho;
+use League\OAuth2\Client\Grant\AbstractGrant;
+
+class Zoho extends AsadZoho
+{
+	/**
+	 * Creates an access token from a response.
+	 *
+	 * The grant that was used to fetch the response can be used to provide
+	 * additional context.
+	 *
+	 * @param  array $response
+	 * @param  AbstractGrant $grant
+	 * @return AccessTokenInterface
+	 */
+	protected function createAccessToken(array $response, AbstractGrant $grant)
+	{
+		return new ZohoAccessToken($response);
+	}
+}
