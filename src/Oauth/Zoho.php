d00c56212d49fe5ade87a4103db8a382f6863fe9
Skullbock <Skullbock@users.noreply.github.com>
2021-03-05 15:29:30 +0000

    Fix styling

--- src/Oauth/Zoho.php
+++ src/Oauth/Zoho.php
@@ -8,18 +8,18 @@ use League\OAuth2\Client\Token\AccessTokenInterface;
 
 class Zoho extends AsadZoho
 {
-	/**
-	 * Creates an access token from a response.
-	 *
-	 * The grant that was used to fetch the response can be used to provide
-	 * additional context.
-	 *
-	 * @param  array $response
-	 * @param  AbstractGrant $grant
-	 * @return AccessTokenInterface
-	 */
-	protected function createAccessToken(array $response, AbstractGrant $grant)
-	{
-		return new ZohoAccessToken($response);
-	}
+    /**
+     * Creates an access token from a response.
+     *
+     * The grant that was used to fetch the response can be used to provide
+     * additional context.
+     *
+     * @param  array $response
+     * @param  AbstractGrant $grant
+     * @return AccessTokenInterface
+     */
+    protected function createAccessToken(array $response, AbstractGrant $grant)
+    {
+        return new ZohoAccessToken($response);
+    }
 }
